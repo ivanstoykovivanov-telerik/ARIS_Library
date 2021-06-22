62ca532ed75cdf8de8a328c86a029ef316f6dbf1
iviv <ivanstoykovivanov-telerik@gmail.com>
2021-06-22 10:32:33 +0100

    Add object output

--- get_Attribute_ID_For_GUID.js
+++ get_Attribute_ID_For_GUID.js
@@ -18,3 +18,35 @@ function getAttrID4GUID(s_GUID){
     return n_returnTypeNum
 }
 
+
+/** function att()
+*  Transfer the specified attribute type GUID to a current attribute type number. 
+ *  returns: attribute type number
+*  Supported formats:
+*  - "f2e211c0-4cf2-11ea-1524-005056849f3d"
+*   - Constant.AT_FUNC
+*  - 8
+*  - "AT_DESC"
+*  - "AT_CUSTOM_DESC" (taken from CustomConstants)
+*/    
+function att(typeNumOrGUID) {
+    var returnTypeNum = null;
+    try {
+        returnTypeNum = ArisData.getActiveDatabase().ActiveFilter().UserDefinedAttributeTypeNum(typeNumOrGUID);  
+        if (returnTypeNum==-1) returnTypeNum = null;         
+    } catch (e) {};
+
+    if ((returnTypeNum == null) && !isNaN(typeNumOrGUID)) {
+        try {
+            returnTypeNum = parseInt(typeNumOrGUID);
+        } catch (e) {}
+    }
+    if (returnTypeNum == null && Constants[typeNumOrGUID]!=null) {
+        returnTypeNum = Constants[typeNumOrGUID];    
+    }   
+    if (returnTypeNum == null && typeof CustomConstants !== 'undefined' && CustomConstants[typeNumOrGUID]!=null ) {
+        returnTypeNum = CustomConstants[typeNumOrGUID];    
+    }      
+    if (null == returnTypeNum) throw typeNumOrGUID + "\nis not valid!";
+    return returnTypeNum;
+}
